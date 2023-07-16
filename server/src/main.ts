bdc43155dd3fef59350b0571cae7cc5ac904d772
João Machado <joaocwd@outlook.com.br>
2023-07-16 17:43:20 -0400

    Ajustando

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});

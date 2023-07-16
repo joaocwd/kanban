de4e54b0a6fe1ba12113acb53bf3b139756daf8e
João Machado <joaocwd@outlook.com.br>
2023-07-16 17:39:54 -0400

    Ajustando

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors();
+  app.use(cors({
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }));
   await app.listen(3000);
 }
 bootstrap();

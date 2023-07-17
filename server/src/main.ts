24f3a6e6ac851d6c43a7f6f0f0ca1ef4f7c3aced
João Machado <37156218+joaocwd@users.noreply.github.com>
2023-07-17 00:37:49 -0400

    Update main.ts
    diretamente do github

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,17 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      /^(.*)/,
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-    credentials: true,
-    allowedHeaders:
-      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
-  })
-  await app.listen(3000);
+  app.enableCors({origin: '*'})
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();

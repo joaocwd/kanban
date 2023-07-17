e966e972e955db0bc32ea93e1b16d50d27cf6cdc
João Machado <37156218+joaocwd@users.noreply.github.com>
2023-07-16 20:02:13 -0400

    Atualizar o main.ts
    fazendo pelo celular

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors({
-    origin: ['https://kanban-production-643d.up.railway.app'],
-  });
   await app.listen(3000);
 }
 bootstrap();
